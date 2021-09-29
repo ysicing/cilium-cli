a68609a609fd12d336c05504773e31c08d23cd76
Jarno Rajahalme <jarno@isovalent.com>
2021-09-29 21:08:48 +0300

    test: Move HTTP validation to a separate flow requirement
    Each flow requirement can match on a single L4 connection. With L7 LB
    feature the HTTP flows can be seen on a separate L4 connection from
    the client TCP connection (which may be terminated by the L7
    LB). Envoy upstream connections may also be kept open so FIN can not
    be seen during a short test. Allow for these conditions by moving HTTP
    requirements to a separate flow requirement where the L7/HTTP filter
    replaces the FIN filter of the client connection.
    
    Signed-off-by: Jarno Rajahalme <jarno@isovalent.com>

--- connectivity/check/action.go
+++ connectivity/check/action.go
@@ -398,6 +398,9 @@ func (a *Action) matchFlowRequirements(ctx context.Context, flows flowsSet, req 
 
 func (a *Action) GetEgressRequirements(p FlowParameters) (reqs []filters.FlowSetRequirement) {
 	var egress filters.FlowSetRequirement
+	var http filters.FlowSetRequirement
+	have_http := false
+
 	srcIP := a.src.Address()
 	dstIP := a.dst.Address()
 
@@ -469,17 +472,6 @@ func (a *Action) GetEgressRequirements(p FlowParameters) (reqs []filters.FlowSet
 					{Filter: filters.And(filters.Or(filters.And(ipRequest, tcpRequest), filters.And(ipResponse, tcpResponse)), filters.Drop()), Msg: "L3/L4 Drop"},
 				},
 			}
-			if a.expEgress.Drop {
-				// L7 drop
-				egress.Middle = append(egress.Middle, filters.FlowRequirement{Filter: filters.And(ipRequest, tcpRequest, filters.L7Drop()), Msg: "L7 Drop"})
-			}
-			if a.expEgress.HTTP.Status != "" || a.expEgress.HTTP.Method != "" || a.expEgress.HTTP.URL != "" {
-				code := uint32(math.MaxUint32)
-				if s, err := strconv.Atoi(a.expEgress.HTTP.Status); err == nil {
-					code = uint32(s)
-				}
-				egress.Middle = append(egress.Middle, filters.FlowRequirement{Filter: filters.And(ipRequest, tcpRequest, filters.HTTP(code, a.expEgress.HTTP.Method, a.expEgress.HTTP.URL)), Msg: "HTTP"})
-			}
 			if p.RSTAllowed {
 				// For the connection termination, we will either see:
 				// a) FIN + FIN b) FIN + RST c) RST
@@ -487,6 +479,23 @@ func (a *Action) GetEgressRequirements(p FlowParameters) (reqs []filters.FlowSet
 				egress.Last = filters.FlowRequirement{Filter: filters.And(filters.Or(filters.And(ipRequest, tcpRequest), filters.And(ipResponse, tcpResponse)), filters.Or(filters.FIN(), filters.RST())), Msg: "FIN or RST", SkipOnAggregation: true}
 			} else {
 				egress.Except = append(egress.Except, filters.FlowRequirement{Filter: filters.And(filters.Or(filters.And(ipRequest, tcpRequest), filters.And(ipResponse, tcpResponse)), filters.RST()), Msg: "RST"})
+			}
+			if a.expEgress.L7Proxy || a.expEgress.HTTP.Status != "" || a.expEgress.HTTP.Method != "" || a.expEgress.HTTP.URL != "" {
+				// HTTP access logs may come from a separate Envoy proxy upstream connection which may be
+				// kept open. Add a separate flow requirement with FIN filter replaced with a L7/HTTP
+				// filter to allow for both a separate connection and a missing FIN.
+				http = egress
+				have_http = true
+				if a.expEgress.Drop {
+					// L7 drop
+					http.Last = filters.FlowRequirement{Filter: filters.And(ipRequest, tcpRequest, filters.L7Drop()), Msg: "L7 Drop"}
+				} else if a.expEgress.HTTP.Status != "" || a.expEgress.HTTP.Method != "" || a.expEgress.HTTP.URL != "" {
+					code := uint32(math.MaxUint32)
+					if s, err := strconv.Atoi(a.expEgress.HTTP.Status); err == nil {
+						code = uint32(s)
+					}
+					http.Last = filters.FlowRequirement{Filter: filters.And(ipRequest, tcpRequest, filters.HTTP(code, a.expEgress.HTTP.Method, a.expEgress.HTTP.URL)), Msg: "HTTP"}
+				}
 			}
 		}
 	case UDP:
@@ -517,6 +526,9 @@ func (a *Action) GetEgressRequirements(p FlowParameters) (reqs []filters.FlowSet
 		reqs = append(reqs, dns)
 	}
 	reqs = append(reqs, egress)
+	if have_http {
+		reqs = append(reqs, http)
+	}
 
 	return reqs
 }
